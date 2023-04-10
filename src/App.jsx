f3b91103ffcd8ddf6dab16e3eb7ce4b28c466311
Miquel <abellagarciamiquel@gmail.com>
2023-04-10 18:25:36 +0200

    get posts

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,36 @@ import { ShoppingList } from "./Components/ShoppingList";
 import { useState } from "react";
 import { LoginForm } from "./Components/LoginForm";
 import { RegisterForm } from "./Components/RegisterForm";
+import { useEffect } from "react";
 
 export const App = () => {
   const [user, setUser] = useState(null);
-  console.log(user)
+  const [shoppingItems, setShoppingItems] = useState([])
+
+  const getShoppingList = async () => {
+    const res = await fetch("http://localhost:4000/getitems", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email: user.email}),
+    });
+    const data = await res.json();
+    setShoppingItems(data.items.shoppingItems)
+  };
+
+  useEffect(() => {
+    if (user) {
+      getShoppingList();
+    }
+  }, [user]);
+
   return (
     <div className="min-h-screen w-full bg-slate-500 flex flex-col items-center">
       {user ? (
         <>
-          <ShoppingForm user={user}/>
-          <ShoppingList user={user}/>
+          <ShoppingForm user={user} />
+          <ShoppingList user={user} shoppingItems={shoppingItems}/>
         </>
       ) : (
         <>
--- src/Components/ShoppingList/index.jsx
+++ src/Components/ShoppingList/index.jsx
@@ -1,34 +1,11 @@
-import React, { useEffect } from "react";
 import { ShoppingItem } from "./ShoppingItem";
 
-export const ShoppingList = ({ user }) => {
-  console.log(user);
-  const getShoppingList = async () => {
-    const res = await fetch("http://localhost:4000/getshoppingitems", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user.email),
-    });
-    const data = await res.json();
-    if (data.ok) {
-      console.log(data);
-    }
-  };
-
-  useEffect(() => {
-    if (user) {
-      getShoppingList();
-    }
-  }, [user]);
-
+export const ShoppingList = ({ shoppingItems }) => {
   return (
     <div className="flex flex-col gap-4 w-1/2 mt-4">
-      <ShoppingItem text={"Test!"} />
-      <ShoppingItem text={"Test!"} />
-      <ShoppingItem text={"Test!"} />
-      <ShoppingItem text={"Test!"} />
+      {shoppingItems.map((item) => {
+        return <ShoppingItem text={item.title} />;
+      })}
     </div>
   );
 };
